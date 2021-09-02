4e2d1a2a1cfb01059b4cebe3434fe5c5b0f549c0
Roshan_29 <mdroshan.0629@gmail.com>
2021-09-02 16:28:03 +0530

    Insurance-Management System

---
+++ insurancemanagement/src/insurance/simpleDateFormat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insurance;
+
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author ROSHAN
+ */
+public class simpleDateFormat extends SimpleDateFormat {
+
+    public simpleDateFormat(String yyyyMMdd) {
+    }
+    
+}
